81d772b90d00a3ae457949ca369e6b976eea11ae
mdmahmud28 <mdmhamud3311@gmail.com>
2026-07-30 23:31:09 +0600

    Add Librarian Books Order Page

---
+++ src/app/dashboard/librarian/books/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LibrarianAllBooksPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LibrarianAllBooksPage;
---
+++ src/app/dashboard/librarian/booksOrder/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BooksOrdrPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BooksOrdrPage;

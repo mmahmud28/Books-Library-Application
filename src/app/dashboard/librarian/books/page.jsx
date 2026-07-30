9eb40b061c992ee03f2489c3524f9b159fc9c8a8
mdmahmud28 <mdmhamud3311@gmail.com>
2026-07-30 23:36:19 +0600

    Add Librarian Books Order Page

--- src/app/dashboard/librarian/books/page.jsx
+++ src/app/dashboard/librarian/books/page.jsx
@@ -1,6 +1,12 @@
+import { useSession } from '@/lib/auth-client';
 import React from 'react';
 
 const LibrarianAllBooksPage = () => {
+
+     const { data: session, isPending } = useSession();
+      const user = session?.user;
+      const userEmail = user?.email;
+    
     return (
         <div>
             

20ca359943eaea603eacf7e5801af02939ba29b9
mdmahmud28 <mdmhamud3311@gmail.com>
2026-08-02 09:01:14 +0600

    Add Admin Actions delete, active, inactive

--- src/app/dashboard/admin/books/page.jsx
+++ src/app/dashboard/admin/books/page.jsx
@@ -1,8 +1,14 @@
 import BookManagementView from "@/app/Components/BookManagementView";
 import { allBooksList } from "@/lib/api/userList";
+import { getUserSession } from "@/lib/core/session";
 
 const AllBookList = async () => {
   const books = await allBooksList();
+
+  const user = await getUserSession();
+  if (!user) {
+    return <div>You do not have permission to view this page.</div>;
+  }
 
   return <BookManagementView initialBooks={books} />;
 };
